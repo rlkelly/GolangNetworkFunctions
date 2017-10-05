eb5205b60c79b8085327d4419e12c5a115f60f21
Robert L Kelly <robert.l.kelly3@gmail.com>
2017-10-04 21:34:35 -0400

    added server code

--- server/server.go
+++ server/server.go
@@ -1,3 +1,29 @@
-package server
+package main
 
-import "net/http/httputil"
+import "net"
+import "fmt"
+import "bufio"
+import "strings" // only needed below for sample processing
+
+func main() {
+
+  fmt.Println("Launching server...")
+
+  // listen on all interfaces
+  ln, _ := net.Listen("tcp", ":8000")
+
+  // accept connection on port
+  conn, _ := ln.Accept()
+
+  // run loop forever (or until ctrl-c)
+  for {
+    // will listen for message to process ending in newline (\n)
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+    // output message received
+    fmt.Print("Message Received:", string(message))
+    // sample process for string received
+    newmessage := strings.ToUpper(message)
+    // send new string back to client
+    conn.Write([]byte(newmessage + "\n"))
+  }
+}
